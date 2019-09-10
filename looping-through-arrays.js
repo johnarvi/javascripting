87cdda3d01efced9d13bd8f1c51c2bd767fce980
johnarvi <arvind.john@mail.utoronto.ca>
2019-09-10 04:55:09 +0000

    Initial commit with all my work so far

---
+++ looping-through-arrays.js
@@ -0,0 +1,6 @@
+let pets = ['cat', 'dog', 'rat'];
+for (let i = 0; i < petlength; i++) {
+   pets[i] = pets[i] +'s';
+}
+
+console.log(pets);
---
+++ revising-strings.js
@@ -0,0 +1,3 @@
+const pizza = 'pizza is alright';
+const pizza = pizza.replace('alright','wonderful');
+console.log(pizza);
